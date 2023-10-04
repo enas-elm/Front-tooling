01dca683ebd5d33ae577a7246643021bf4e5e52d
Enas <enas.elmershati@gmail.com>
2023-10-04 15:21:51 +0200

    Changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-    './index.html',
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      'primary': '#027b80',
-      'primaryLight': '#B3ECEC',
-      'primaryDark': '#00484F',
+	content: ["./src/**/*.{html,js}", "./index.html"],
+	theme: {
+		extend: {},
+		colors: {
+			primary: "#027b80",
+			primaryLight: "#B3ECEC",
+			primaryDark: "#00484F",
 
-      'secondary': '#af4d4d',
-      'secondaryLight': '#EBC6C6',
-      'secondaryDark': '#7D0000',
+			secondary: "#af4d4d",
+			secondaryLight: "#EBC6C6",
+			secondaryDark: "#7D0000",
 
-      'tertiary': '#A2CF8C',
-      'tertiaryLight': '#e3fad8',
-      'tertiaryDark': '#5C8149',
-
-    },
-    screens: {
-      xs: '480px',
-    },
-  },
-  plugins: [],
+			tertiary: "#A2CF8C",
+			tertiaryLight: "#e3fad8",
+			tertiaryDark: "#5C8149",
+		},
+		screens: {
+			xs: "480px",
+		},
+	},
+	plugins: [],
 };
