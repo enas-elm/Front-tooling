3fe7d1805b9bb74fd1b32ea31b0141204206c61e
Enas <enas.elmershati@gmail.com>
2023-10-04 15:25:28 +0200

    Indent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}",
-    './index.html',
-  ],
-  theme: {
-    extend: {},
-    colors: {
-      'primary': '#027b80',
-      'primaryLight': '#B3ECEC',
-      'primaryDark': '#00484F',
+	content: ["./src/**/*.{html,js}",
+		'./index.html',
+	],
+	theme: {
+		extend: {},
+		colors: {
+			'primary': '#027b80',
+			'primaryLight': '#B3ECEC',
+			'primaryDark': '#00484F',
 
-      'secondary': '#af4d4d',
-      'secondaryLight': '#EBC6C6',
-      'secondaryDark': '#7D0000',
+			'secondary': '#af4d4d',
+			'secondaryLight': '#EBC6C6',
+			'secondaryDark': '#7D0000',
 
-      'tertiary': '#A2CF8C',
-      'tertiaryLight': '#e3fad8',
-      'tertiaryDark': '#5C8149',
+			'tertiary': '#A2CF8C',
+			'tertiaryLight': '#e3fad8',
+			'tertiaryDark': '#5C8149',
 
-    },
-    screens: {
-      xs: '480px',
-    },
-  },
-  plugins: [],
+		},
+		screens: {
+			xs: '480px',
+		},
+	},
+	plugins: [],
 };
