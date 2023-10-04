3419c837a49c7481c82e8bf9fb9736680aa71199
Enas <enas.elmershati@gmail.com>
2023-10-04 10:13:36 +0200

    Config tailwind variables :wrench:

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Tailwind.css
 Run "npm run tailwind:watch" to run tailwind in the project
+the variable are configured in the tailwind.config.js, you can change bthe values
 
 Prettier
 Run "run npx prettier . --write to"
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,23 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'primary': '#027b80',
+      'primaryLight': '#B3ECEC',
+      'primaryDark': '#00484F',
+
+      'secondary': '#af4d4d',
+      'secondaryLight': '#EBC6C6',
+      'secondaryDark': '#7D0000',
+
+      'tertiary': '#A2CF8C',
+      'tertiaryLight': '#e3fad8',
+      'tertiaryDark': '#5C8149',
+
+    },
+    screens: {
+      xs: '480px',
+    },
   },
   plugins: [],
 };
